fc176ebc2035ac258b977befa5a24d5a4bb287d7
mrs1669 <mrs1669.develop@gmail.com>
2021-12-13 04:39:14 +0900

    delete 2

--- settings.py
+++ settings.py
@@ -3,12 +3,6 @@ from os import environ
 
 SESSION_CONFIGS = [
     dict(
-        name='guess_two_thirds',
-        display_name="Guess 2/3 of the Average",
-        app_sequence=['guess_two_thirds', 'payment_info'],
-        num_demo_participants=3,
-    ),
-    dict(
         name='survey',
         display_name="サンプル調査",
         app_sequence=['survey', 'payment_info'], 
@@ -18,7 +12,7 @@ SESSION_CONFIGS = [
         name='prisoner',
         display_name="囚人のジレンマ",
         app_sequence=['prisoner', 'payment_info'], 
-        num_demo_participants=2,
+        num_demo_participants=2
     )
 ]
 
