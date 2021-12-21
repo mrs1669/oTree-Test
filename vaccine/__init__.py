f21144afffd01bd189e55b5bbcf3f3a7de335daa
mrs1669 <mrs1669.develop@gmail.com>
2021-12-21 14:09:15 +0900

    update basic information

--- vaccine/__init__.py
+++ vaccine/__init__.py
@@ -28,7 +28,9 @@ class Player(BasePlayer):
         widget=widgets.RadioSelect,
     )
     name = models.StringField(label='あなたの氏名をフルネームでお書きください。')
-    studentNumber = models.IntegerField(label='あなたの学籍番号をお書きください。(b,gを除き半角数字で入力してください)', min=1000000, max=2200000)
+    studentNumber = models.StringField(label='あなたの学籍番号をお書きください。')
+    email = models.StringField(label='あなたの電子メールアドレスをお書きください。なお、大学から与えられている公式アカウントのものをご記入ください。それ以外のものを記入した場合、謝金は支払われません。')
+    studentNumberTest = models.IntegerField(label='あなたの学籍番号をお書きください。(b,gを除き半角数字で入力してください)', min=1000000, max=2200000)
     q2_1 = models.StringField(
         choices=[['yes', 'はい、わかりやすかったです'], ['no', 'いいえ、わかりにくいところがありました']],
         label='先ほどの実験の説明は理解しやすいものでしたか？',
@@ -56,6 +58,9 @@ class Demographics(Page):
     form_model = 'player'
     form_fields = ['age', 'gender','name','studentNumber']
 
+class BasicInformation(Page):
+    form_model = 'player'
+    form_fields = ['name','studentNumber','email']
 
 class CognitiveReflectionTest(Page):
     form_model = 'player'
@@ -75,4 +80,4 @@ class Introduction(Page):
         if values['isConsent'] == 'disagree':
             return '同意していただけない場合は実験に進むことはできません。ブラウザを閉じて実験を終了してください。'
 
-page_sequence = [Introduction, FF, CognitiveReflectionTest]
+page_sequence = [Introduction, BasicInformation, FF, CognitiveReflectionTest]
